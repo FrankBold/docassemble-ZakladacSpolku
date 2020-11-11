76428d41f1b33d8e413869e173ab91f5ae04be7f
Michal Kuk <michal.kuk@frankbold.org>
2020-11-11 17:57:11 +0100

    Úprava checklistu

--- docassemble/ZakladacSpolku/utility.py
+++ docassemble/ZakladacSpolku/utility.py
@@ -15,3 +15,15 @@ def contains_spolek(x):
 def string_pole(x):
   x = x.split('\r\n')
   return x
+
+def ziskejPolozky(kat):
+  page = requests.get("https://da-test.frankbold.org/playgroundstatic/Zakladac/1/checklist.json")
+  y = json.loads(page.content)
+  list_duvody = {}
+
+  for x in y['checklist']:
+    if x["kategorie"] == kat:
+      if x["hodnota"] == value(str(x["podminka"])):
+        list_duvody[x["id"]] = x["text"]
+
+  return list_duvody
