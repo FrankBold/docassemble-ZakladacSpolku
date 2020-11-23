c70d5ecefcf2fe716816ec9359e3f0cf8633aed7
Michal Kuk <michal.kuk@frankbold.org>
2020-11-23 19:41:19 +0100

    asdasdf

--- docassemble/ZakladacSpolku/utility.py
+++ docassemble/ZakladacSpolku/utility.py
@@ -7,12 +7,12 @@ def contains_spolek(x):
   x = x.lower()
   if "spolek" in x:
     return True
-  elif "z.s." in x:
+  elif "z. s." in x:
     return True
   elif "zapsaný spolek" in x:
     return True
   else:
-    validation_error('Název spolku <strong>musí</strong> obsahovat "z.s.", "spolek", nebo "zapsaný spolek"')
+    validation_error('Název spolku <strong>musí</strong> obsahovat "z. s.", "spolek", nebo "zapsaný spolek"')
   return
 
 def string_pole(x):
