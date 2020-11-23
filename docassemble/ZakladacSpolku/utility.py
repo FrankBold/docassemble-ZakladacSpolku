358c15b7273428f5cb75cac045b342fd6a4f812a
Michal Kuk <michal.kuk@frankbold.org>
2020-11-23 19:39:40 +0100

    Kontrola jedinečnosit názvu

--- docassemble/ZakladacSpolku/utility.py
+++ docassemble/ZakladacSpolku/utility.py
@@ -1,6 +1,7 @@
 from docassemble.base.util import validation_error, value
 import json
 import requests
+import xmltodict
 
 def contains_spolek(x):
   x = x.lower()
@@ -29,3 +30,27 @@ def ziskejPolozky(kat):
         list_duvody[x["id"]] = x["text"]
 
   return list_duvody
+
+def overitXml(firma):
+  URL = 'https://wwwinfo.mfcr.cz/cgi-bin/ares/darv_std.cgi'
+  params = {'obchodni_firma': firma}
+  page = requests.get(URL, params=params)
+  page.encoding = 'utf-8'
+  ares_data = xmltodict.parse(page.text)
+  response_root_wrapper = ares_data['are:Ares_odpovedi']
+  response_root = response_root_wrapper['are:Odpoved']
+  number_of_results = response_root['are:Pocet_zaznamu']
+
+  info = []
+
+  if int(number_of_results) == 0:
+    return false
+  elif int(number_of_results) == 1:
+    company_record = response_root['are:Zaznam']
+    info.append(company_record.get('are:Obchodni_firma'))
+  else:
+    info = []
+    company_record = response_root['are:Zaznam']
+    for zaznam in company_record:
+      info.append(zaznam.get('are:Obchodni_firma'))
+  return info
